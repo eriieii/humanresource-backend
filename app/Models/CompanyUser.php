bfec5bbca54389f31f89cfefe7150be8b52236b6
EriFerdian23 <eri.ferdian@duapuluhtiga.com>
2023-12-19 19:54:30 +0700

    first

---
+++ app/Models/CompanyUser.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CompanyUser extends Model
+{
+    use HasFactory;
+    protected $table = 'company_user';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Company;
+use App\Models\Team;
+use App\Models\Role;
+use App\Models\Employee;
+use App\Models\Responsibility;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+        Company::factory(3)->create();
+        Team::factory(10)->create();
+        Role::factory(5)->create();
+        Employee::factory(10)->create();
+        Responsibility::factory(5)->create();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
